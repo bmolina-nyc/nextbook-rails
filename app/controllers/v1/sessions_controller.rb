98052240ab1ad2d3b3b496d587597b13656ed844
Linus Rosskamp <linus.rosskamp@gmail.com>
2017-09-12 20:04:12 -0400

    Generate sessions controller. Fix routes for sessions controller. Add to session controller actions

---
+++ app/controllers/v1/sessions_controller.rb
@@ -0,0 +1,30 @@
+class V1::SessionsController < ApplicationController
+
+  # GET /v1/session
+  def create
+    @user = User.find_by(email: params[:email].downcase)
+
+    if @user&.valid_password?(params[:password])
+      render :create, status: :created
+    else
+      head :unauthorized
+    end
+  end
+
+  # POST /v1/session
+  def show
+    current_user ? head(:ok) : head(:unauthorized)
+  end
+
+  # DELETE /v1/session
+  def destroy
+    nilify_token
+    current_user.save ? head(:ok) : head(:unauthorized)
+  end
+
+  private
+
+  def nilify_token
+    current_user&.authentication_token = nil
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   namespace :v1 do
     resources :users, only: %i(create show update destroy)
     resources :user_books, only: %i(create destroy)
+    resource :sessions, only: %i(create show destroy)
   end
 
   devise_for :users
