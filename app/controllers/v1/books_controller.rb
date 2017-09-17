dc018df3a18e7461028b93719155bdc1536a9e3e
Linus Rosskamp <linus.rosskamp@gmail.com>
2017-09-17 18:04:13 -0400

    update user_books controller and add cache

--- app/controllers/v1/books_controller.rb
+++ app/controllers/v1/books_controller.rb
@@ -2,16 +2,26 @@ class V1::BooksController < ApplicationController
 
   # GET /v1/books
   def index
-      @books = current_user.book_ids(10).map do |google_id|
-      next if params[:id] && params[:ids].includes(google_id)
-      response = lookup(google_id)
-      hash = parse_json_into_hash(response)
-      build_response(hash)
+    @books = current_user.book_ids(15).map do |google_id|
+      # next if exists_on_client?(google_id)
+      parsed = Rails.cache.fetch("#{google_id}-lookup", expires_in: 30.days) do
+        fetch_and_parse(google_id)
+      end
+      build_response(parsed)
     end
     render json: { books: @books.as_json }, status: :ok
   end
 
   private
+
+  def exists_on_client?(google_id)
+    !!params[:ids] && params[:ids].include?(google_id)
+  end
+
+  def fetch_and_parse(google_id)
+    response = lookup(google_id)
+    parse_json_into_hash(response)
+  end
 
   def lookup(google_id)
     GoogleBooksApi::Requester::Lookup.new(google_id).call
@@ -21,7 +31,7 @@ class V1::BooksController < ApplicationController
     GoogleBooksApi::JsonParser::Lookup.new(response).call
   end
 
-  def build_response(hash)
-    GoogleBooksApi::ResponseBuilder::Lookup.new(current_user, hash).call
+  def build_response(parsed)
+    GoogleBooksApi::ResponseBuilder::Lookup.new(current_user, parsed).call
   end
 end
