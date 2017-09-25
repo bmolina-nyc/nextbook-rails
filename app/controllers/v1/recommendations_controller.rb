5cba2c90562b739557dd7278f0ae111f1f6fde4d
Linus Rosskamp <linus.rosskamp@gmail.com>
2017-09-25 14:31:01 -0400

    Remove cache. Add puts debuggers

--- app/controllers/v1/recommendations_controller.rb
+++ app/controllers/v1/recommendations_controller.rb
@@ -2,16 +2,16 @@ class V1::RecommendationsController < ApplicationController
 
   # GET v1/recommendations
   def index
-    books = Rails.cache.fetch(
-      "#{params[:title].downcase}-Rec", expires_in: 10.days) do
+    # books = Rails.cache.fetch(
+      # "#{params[:title].downcase}-Rec", expires_in: 10.days) do
       titles_array = fetch_and_parse_recommendations(params[:title])
       books = []
       titles_array.map do |title|
         books << fetch_and_parse_from_google_books_api(title)
         break if books.length == 3
       end
-      books
-    end
+      # books
+    # end
 
     render json: camelize_books(books), status: :ok
   end
@@ -20,14 +20,18 @@ class V1::RecommendationsController < ApplicationController
 
   def fetch_and_parse_recommendations(title)
     url = TasteDiveApi::UrlGenerator.new(title).call
+    puts "TD: url: #{url}"
     response = Requester.new(url).call
     TasteDiveApi::JsonParser.new(response).call
+    puts "TD: response: #{response}"
   end
 
   def fetch_and_parse_from_google_books_api(title)
     url = GoogleBooksApi::UrlGenerator::Recommendation.new(title).call
+    puts "GB: url: #{url}"
     response = Requester.new(url).call
     GoogleBooksApi::JsonParser::Recommendation.new(response).call
+    puts "GB: response: #{response}"
   end
 
   def camelize_books(books)
