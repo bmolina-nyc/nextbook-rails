63cc61fc22f807bd143d78dc5f324c1016be283f
lrosskamp <linus.rosskamp@gmail.com>
2017-11-18 11:26:44 -0500

    Optimize demo UserBook creation to 1 SQL statement

--- app/services/demo/create_user_books.rb
+++ app/services/demo/create_user_books.rb
@@ -4,22 +4,26 @@ class Demo::CreateUserBooks
   end
 
   def call
-    Demo::Data::USER_BOOKS.map do |user_book|
-      user.user_books.create(user_book_params(user_book))
-    end
+    ActiveRecord::Base.connection.execute(sql_string)
   end
 
   private
 
-  def user_book_params(user_book)
-    timestamp = 10.minutes.ago
-    {
-      user_id: user.id,
-      google_id: user_book[:google_id],
-      status:  user_book[:status],
-      updated_at: timestamp,
-      created_at: timestamp
-    }
+  def sql_string
+    string = 'INSERT INTO user_books '
+    string += "(status, user_id, google_id, created_at, updated_at) VALUES #{values}"
+  end
+
+  def values
+    values = Demo::Data::USER_BOOKS.map do |user_book|
+      time = timestamp
+      "(#{UserBook.statuses[user_book[:status]]}, #{user.id}, '#{user_book[:google_id]}', '#{time}', '#{time}')"
+    end
+    values.join(',')
+  end
+
+  def timestamp
+    10.minutes.ago.strftime "%Y-%m-%d %H:%M:%S.%6N"
   end
 
   attr_reader :user
